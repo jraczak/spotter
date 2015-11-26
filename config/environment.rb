b52885430e98d2d41576ef81063ebeb975693408
Justin Raczak <jraczak@gmail.com>
2015-11-25 19:12:20 -0800

    Add sendgrid config in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set up the SendGrid configs
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'spotterapp.io',
+  :enable_starttls_auto => true
+}
