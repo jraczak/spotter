18c303d388f9f4c5086014b46151da4c9372bc66
Justin Raczak <jraczak@gmail.com>
2015-11-25 20:07:58 -0800

    fix mailer_sender to valid format

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'The Spotter Team'
+  config.mailer_sender = '"The Spotter Team" <hello@spotter.io>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
